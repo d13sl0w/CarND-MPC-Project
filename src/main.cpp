7a9098f6b4d54a7be2cc76d77e126c6f79bbbf9b
dieslow <doug.brummell@gmail.com>
2017-07-04 15:20:04 -0400

    submit

--- src/main.cpp
+++ src/main.cpp
@@ -179,6 +179,8 @@ int main() {
                     //
                     // NOTE: REMEMBER TO SET THIS TO 100 MILLISECONDS BEFORE
                     // SUBMITTING.
+
+                    // SEE README FOR FURTHER DISCUSSION OF LATENCY AND MODELING CHOICES
                     this_thread::sleep_for(chrono::milliseconds(100));
                     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
                 }
